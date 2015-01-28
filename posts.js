9f63fee368d525cc786aa82cc0169fb19725cef8
pavani551 <pasula.pavani@gmail.com>
2015-01-28 15:00:30 +0530

    kellll

---
+++ posts.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var Schema       = mongoose.Schema;
+
+mongoose.connect('mongodb://localhost:27017/t', function (error) {
+    if (error) {
+        console.log(error);
+    }
+});
+
+var posts   = new Schema({
+    username: { type: String, required: true, trim:true  },
+    password : { type: String, required: true, trim:true  },
+    
+});
+var UserModel = mongoose.model('posts', posts);
+
+module.exports = UserModel ;
