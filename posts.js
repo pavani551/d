d6ba896f5f404a483b75a6b43713010f3e53fc77
krgs3 <swethakrgs@gmail.com>
2015-01-28 15:32:15 +0530

    Update posts.js
    gfgfggfggf

--- posts.js
+++ posts.js
@@ -5,7 +5,7 @@ mongoose.connect('mongodb://localhost:27017/t', function (error) {
     if (error) {
         console.log(error);
     }
-});
+});hello
 
 var posts   = new Schema({
     username: { type: String, required: true, trim:true  },
