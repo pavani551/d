dbf8d5bdb007474f8305029518face98a6f18ce3
pavani551 <pasula.pavani@gmail.com>
2015-01-28 15:34:49 +0530

    Update posts.js
    added  ur name

--- posts.js
+++ posts.js
@@ -6,7 +6,7 @@ mongoose.connect('mongodb://localhost:27017/t', function (error) {
         console.log(error);
     }
 });hello
-
+hai swetha
 var posts   = new Schema({
     username: { type: String, required: true, trim:true  },
     password : { type: String, required: true, trim:true  },
